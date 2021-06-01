c230987a94769fed8553815e68f7023dd0896ea0
unknown <fukasawa.ryoya@gmail.com>
2021-06-01 15:31:27 +0900

    first commit

--- app/Http/Controllers/CircleController.php
+++ app/Http/Controllers/CircleController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\models\Circle;
+use App\Models\Circle;
 
 class CircleController extends Controller {
     public function show() {
