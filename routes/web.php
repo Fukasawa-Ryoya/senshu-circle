43e755206a667e32a1aad4d0e52996a11e2914a0
Fukasawa-Ryoya <85042911+Fukasawa-Ryoya@users.noreply.github.com>
2021-06-02 15:06:50 +0900

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -12,11 +12,11 @@
 */
 
 
-Route::get('/','CircleController@show');
+Route::GET('/','CircleController@show');
 
-Route::get('/circle/{id}','CircleController@showDetail');
+Route::GET('/circle/{id}','CircleController@showDetail');
 
-Route::get('/category/{category}','CircleController@showCategory');
-Route::get('/{type}','CircleController@showType');
+Route::GET('/category/{category}','CircleController@showCategory');
+Route::GET('/{type}','CircleController@showType');
 
 Route::POST('/result','CircleController@search');
