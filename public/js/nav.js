288f9d6eca635abe321ad257318743646bf2b601
Fukasawa Ryoya <fukasawa.ryoya@gmail.com>
2021-06-03 12:35:32 +0900

    third commit

--- public/js/nav.js
+++ public/js/nav.js
@@ -57,7 +57,7 @@ $(".nav-menu").on("click", () => {
             })
             // navButton内の.attr('class')のためにつけている
             .addClass("active");
-        $(".menu").animate({ marginTop: "300px" }, 400).addClass("menu-active");
+        $(".menu").animate({ marginTop: "999px" }, 400).addClass("menu-active");
         // menuが開いているときはサークル詳細ページに遷移させなくする。
         $(".circle-wrap a").on("click", (e) => {
             e.preventDefault();
