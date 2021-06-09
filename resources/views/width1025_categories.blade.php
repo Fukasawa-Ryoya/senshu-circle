abdffcfc8e904a85ead0b7648a71ac2699961526
Fukasawa Ryoya <fukasawa.ryoya@gmail.com>
2021-06-09 14:15:44 +0900

    commit 0609

--- resources/views/width1025_categories.blade.php
+++ resources/views/width1025_categories.blade.php
@@ -2,10 +2,5 @@
   <h3 class="blade-menu-title">カテゴリー</h3>
   <div class="blade-category-wrap">
     @include('categories')
-    <ul class="categories">
-      <li class="category-title"><a href="/inquiry">
-          <p class="category">お問い合わせ</p>
-        </a></li>
-    </ul>
   </div>
 </div>
